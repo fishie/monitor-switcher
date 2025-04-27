afb10b579096a81402d454251cc35d9fe53f3403
Rishie Sharma <rishie@rishie.se>
2025-04-27 22:30:09 +0200

    Refactor

---
+++ MonitorSwitcherGui/SendMessageAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MonitorSwitcherGui
+{
+    public class SendMessageAPI
+    {
+        public const int HWND_BROADCAST = 0xFFFF;
+        public const int WM_SYSCOMMAND   = 0x0112;
+        public const int SC_MONITORPOWER = 0xf170;
+
+        public const int MONITOR_ON = -1;
+        public const int MONITOR_OFF = 2;
+        public const int MONITOR_STANBY = 1;
+
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr GetConsoleWindow();
+    }
+}
