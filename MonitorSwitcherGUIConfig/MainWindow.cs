8bf60f9d4fdd306cc33bb9718befdb5575bc274d
Rishie Sharma <rishie@rishie.se>
2025-04-28 13:33:14 +0200

    More modernize

--- MonitorSwitcherGUIConfig/MainWindow.cs
+++ MonitorSwitcherGUIConfig/MainWindow.cs
@@ -1,59 +1,55 @@
-﻿using System;
-using System.IO;
-using System.Windows.Forms;
-using MonitorSwitcher;
+﻿using MonitorSwitcher;
 
-namespace MonitorSwitcherGUI
+namespace MonitorSwitcherGUIConfig;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    public MainForm()
     {
-        public MainForm()
+        InitializeComponent();
+    }
+
+    private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        UpdateProfileList();
+    }
+
+    private void UpdateProfileList()
+    {
+        string settingsDirectory = DisplaySettings.GetSettingsDirectory(null);
+        string settingsDirectoryProfiles = DisplaySettings.GetSettingsProfileDirectory(settingsDirectory);
+
+        // get profiles
+        string[] profiles = Directory.GetFiles(settingsDirectoryProfiles, "*.xml");
+        foreach (string profile in profiles)
         {
-            InitializeComponent();
+            string itemCaption = Path.GetFileNameWithoutExtension(profile);
+            lbProfiles.Items.Add(itemCaption);
         }
 
-        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
-        {
+        UpdateGUIStatus();
+    }
 
-        }
+    private void UpdateGUIStatus()
+    {
+        tsbAdd.Enabled = true;
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            UpdateProfileList();
-        }
+        tsbDelete.Enabled = (lbProfiles.SelectedItem != null);
+        tsbExport.Enabled = (lbProfiles.SelectedItem != null);
+    }
 
-        private void UpdateProfileList()
-        {
-            string settingsDirectory = DisplaySettings.GetSettingsDirectory(null);
-            string settingsDirectoryProfiles = DisplaySettings.GetSettingsProfileDirectory(settingsDirectory);
+    private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+    {
+        
+    }
 
-            // get profiles
-            string[] profiles = Directory.GetFiles(settingsDirectoryProfiles, "*.xml");
-            foreach (string profile in profiles)
-            {
-                string itemCaption = Path.GetFileNameWithoutExtension(profile);
-                lbProfiles.Items.Add(itemCaption);
-            }
-
-            UpdateGUIStatus();
-        }
-
-        private void UpdateGUIStatus()
-        {
-            tsbAdd.Enabled = true;
-
-            tsbDelete.Enabled = (lbProfiles.SelectedItem != null);
-            tsbExport.Enabled = (lbProfiles.SelectedItem != null);
-        }
-
-        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-            
-        }
-
-        private void lbProfiles_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            UpdateGUIStatus();
-        }
+    private void lbProfiles_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        UpdateGUIStatus();
     }
 }
--- MonitorSwitcherGUIConfig/Program.cs
+++ MonitorSwitcherGUIConfig/Program.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Windows.Forms;
+﻿namespace MonitorSwitcherGUIConfig;
 
-namespace MonitorSwitcherGUI
+static class Program
 {
-    static class Program
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            if (Environment.OSVersion.Version.Major >= 6)
-                SetProcessDPIAware();
+        if (Environment.OSVersion.Version.Major >= 6)
+            SetProcessDPIAware();
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
-
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
     }
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern bool SetProcessDPIAware();
+
 }
